2c0e3194ac9adf08b4e6f0e6ed7e653267a31c56
Saba Antadze <chroll09@yahoo.com>
2025-12-05 15:27:21 +0400

    feat: add demo payment modules

---
+++ backend/src/modules/payment/payment.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { PrismaService } from "../../prisma/prisma.service";
+
+@Injectable()
+export class PaymentService {
+  constructor(private prisma: PrismaService) {}
+
+  // 1) create fake payment
+  async createPayment(orderId: number) {
+    const order = await this.prisma.order.findUnique({
+      where: { id: orderId },
+    });
+
+    if (!order) throw new NotFoundException("Order not found");
+
+    return {
+      paymentId: "fake_" + Date.now(),
+      amount: order.total,
+      currency: "USD",
+      status: "PENDING",
+    };
+  }
+
+  // 2) confirm fake payment → update order
+  async confirmPayment(paymentId: string, orderId: number) {
+    if (!paymentId.startsWith("fake_"))
+      throw new NotFoundException("Invalid payment");
+
+    const updated = await this.prisma.order.update({
+      where: { id: orderId },
+      data: { status: "PAID" },
+    });
+
+    return {
+      message: "Payment confirmed",
+      order: updated,
+    };
+  }
+}
