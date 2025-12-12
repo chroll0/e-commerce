fa18066f393b3a10dfc3642f52d2f6ae513c3900
Saba Antadze <chroll09@yahoo.com>
2025-12-12 15:29:35 +0400

    Feat: add necessary backend dependencies for user

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
 
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    allowedHeaders: "Content-Type, Authorization",
-    methods: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
   });
 
   const port = process.env.PORT || 3001;
