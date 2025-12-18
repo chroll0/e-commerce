138e680d7b0114b4f384bddd3caa9af22f8b5108
Saba Antadze <chroll09@yahoo.com>
2025-12-18 17:42:40 +0400

    fix(logo):change logo component styles

--- frontend/src/components/ui/Logo.tsx
+++ frontend/src/components/ui/Logo.tsx
@@ -2,11 +2,15 @@ import Link from "next/link";
 
 const Logo = () => {
   return (
-    <Link href="/" className="flex items-center gap-0.5 select-none">
-      <span className="text-2xl font-extrabold text-primary tracking-tight">
-        S
+    <Link
+      href="/"
+      className="relative flex items-baseline select-none tracking-wider"
+    >
+      <span className="text-3xl font-bold text-primary leading-none">S</span>
+
+      <span className="relative text-lg font-medium tracking-wider text-foreground left-0 bottom-[0.1rem] underline decoration-1">
+        atori
       </span>
-      <span className="text-xl font-semibold text-primary">atori</span>
     </Link>
   );
 };
